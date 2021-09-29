e556a8e8cbdd57009368fa74cdc7d94c89e22de5
Jeyoung117 <capricorn116117@gmail.com>
2021-09-29 17:43:50 +0900

    Add unpackproposal method

--- src/main/java/org/sslab/adapter/Corfu_access.java
+++ src/main/java/org/sslab/adapter/Corfu_access.java
@@ -1,4 +1,140 @@
 package org.sslab.adapter;
 
+import com.google.common.reflect.TypeToken;
+import com.google.protobuf.ByteString;
+import io.grpc.stub.StreamObserver;
+import org.corfudb.protocols.wireprotocol.Token;
+import org.corfudb.runtime.CorfuRuntime;
+import org.corfudb.runtime.collections.CorfuTable;
+import org.corfudb.runtime.object.transactions.Transaction;
+import org.corfudb.runtime.object.transactions.TransactionType;
+import org.corfudb.runtime.view.AddressSpaceView;
+import org.corfudb.runtime.view.stream.IStreamView;
+import org.hyperledger.fabric.protos.peer.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.logging.Logger;
+
+/**
+ * @author Jeyoung Hwang.capricorn116@postech.ac.kr
+ *         created on 2021. 9. 28.
+ */
 public class Corfu_access {
+    Map<UUID, CorfuRuntime> runtimes;
+    Map<UUID, IStreamView> streamViews;
+    Map<String, Long> lastReadAddrs;
+    //tokenMap key: fabric txID, value: access token
+    Map<String, Token> tokenMap;
+
+    public Corfu_access() {
+        streamViews = new HashMap<UUID, IStreamView>();
+        runtimes = new HashMap<UUID, CorfuRuntime>();
+//        runtime = new CorfuRuntime(runtimeAddr[0]).connect();
+        lastReadAddrs = new HashMap<String, Long>();
+        System.out.println("Init AdapterModuleService");
+        tokenMap = new HashMap<String, Token>();
+    }
+
+
+    private static CorfuRuntime getRuntimeAndConnect(String configurationString) {
+        CorfuRuntime corfuRuntime = new CorfuRuntime(configurationString).connect();
+        return corfuRuntime;
+    }
+    CorfuRuntime runtime =  getRuntimeAndConnect("141.223.121.251:12011");
+    AddressSpaceView addressSpaceView = runtime.getAddressSpaceView();
+
+    private final Logger logger = Logger.getLogger(AdapterModuleService.class.getName());
+
+
+    public void getStringState(String objectKey, String channelID, String chaincodeID) {
+        Token current_token = runtime.getSequencerView().query().getToken();
+        long blockNum = current_token.getSequence();
+
+        Map<String, byte[]> map = runtime.getObjectsView()
+                .build()
+                .setStreamName(channelID + chaincodeID + objectKey)     // stream ID
+                .setTypeToken(new TypeToken<CorfuTable<String, byte[]>>() {
+                })
+                .open();
+
+//        ByteString data = ByteString.copyFrom(map.get(objectKey));
+        byte[] temp = map.get(objectKey);
+        if (temp == null) {
+
+            System.out.println("[peer-interface] {getState} success");
+
+        } else {
+//            System.out.println(temp);
+            ByteString tempbs = ByteString.copyFrom(temp);
+
+            System.out.println("[peer-interface] {getState} success");
+        }
+    }
+
+
+    public void putStringState(String objectKey, String channelID, String chaincodeID, byte[] data) {
+        String Success_msg = "Putstate success ";
+
+        System.out.println("[peer-interface] {putstate} Corfu runtime is connected");
+        Map<String, byte[]> map = runtime.getObjectsView()
+                .build()
+                .setStreamName(channelID + chaincodeID + objectKey)     // stream ID
+                .setTypeToken(new TypeToken<CorfuTable<String, byte[]>>() {
+                })
+                .open();
+
+        map.put(objectKey, data);
+
+        System.out.println("[peer-interface] {putState} success");
+    }
+
+    public void issueSnapshotToken() {
+        Token current_token = runtime.getSequencerView().query().getToken();
+        Transaction vCorfutx = runtime.getObjectsView()
+                .TXBuild()
+                .type(TransactionType.OPTIMISTIC)
+                .runtime(runtime)
+                .snapshot(current_token)
+                .build();
+        vCorfutx.begin();
+
+//        tokenMap.put(request.getTemp(), current_token);
+
+        System.out.println("[peer-interface] {issueSnapshotToken} success");
+    }
+
+    public void commitTransaction() {
+//    public void commitTransaction(ReqCommit request, StreamObserver<ResCommit> responseObserver) {
+//        System.out.println("[peer-interface] {commitTransaction} Corfu runtime is connected");
+//        System.out.println("전송받은 request: " + request);
+//        System.out.println(request.getTest());
+        Token current_token = runtime.getSequencerView().query().getToken();
+//        UUIfD streamID = CorfuRuntime.getStreamID("mychannel"); //추후 실제 channel ID로 변
+//        IStreamView sv = streamViews.get(streamID);
+
+        long appended_add = runtime.getObjectsView().TXEnd();
+
+
+//            long logicalAddr = sv.append(request.toByteArray());
+//        System.out.println("[peer-interface] {commitTransaction} ProposalResponse size: " + request.getSerializedSize());
+//        System.out.println("[peer-interface] {commitTransaction} appended_add: " + appended_add);
+//            System.out.println("[peer-interface] {commitTransaction} logical addr: " + logicalAddr);
+
+//            if(logicalAddr >= 0) {
+
+//            System.out.println("[peer-interface] {commitTransaction} return success");
+//            }
+//            else {
+//                reply = ResCommit.newBuilder()
+//                        .setSuccess(false)
+//                        .build();
+//                System.out.println("[peer-interface] {commitTransaction} return fail");
+//            }
+
+
+//        System.out.println("appended_add:" + appended_add);
+        System.out.println("[peer-interface] {commitTransaction} Corfu runtime is finished");
+    }
 }
